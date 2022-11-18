3f9b10f1e8374d6879bd95b9f38d08089cd2865b
xinyuanliu <window_9@163.com>
2022-11-18 10:50:01 +0800

    ok

--- ok.js
+++ ok.js
@@ -17,12 +17,12 @@ function convertJson(originData, data) {
 }
 
 function convertString(originValue, data) {
-    return originValue.replaceAll(/{{\w+\.[\w.]+}}/g, (_, key) => {
+    return originValue.replaceAll(/{{\w+\.([\w.]+)}}/g, (_, key) => {
         const kdata = getDataFromJsonByKeyChain(key, data);
         const typeofData = {}.toString.call(kdata);
-        return typeofData === '[object Array]' || typeofData === '[object Object]'
-            ? JSON.stringify(kdata)
-            : kdata;
+        if (typeofData === '[object Array]' || typeofData === '[object Object]')
+            return JSON.stringify(kdata);
+        return typeof kdata === 'string' ? `${kdata}` : kdata;
     });
 }
 
@@ -50,29 +50,32 @@ function getDataFromJsonByKeyChain(keyChain, data) {
 
     if (!keys) return '';
 
-    keys.shift();
-
     let result = data;
     keys.forEach((key) => {
-      result = result[key];
+        result = result[key];
     });
     return result;
 }
 
-// 占位符变量替换成值
-function replaceParam(str, data) {
-    const keys = str.match(/(?=<{{)?(\w+(?=\.)?)+(?=}})?/g);
+const params = {
+    mp_app_id: '123',
+    query_page_num: 1,
+    query_page_size: 10,
+    business: {
+        code: '800050',
+    },
+};
 
-    if (!keys) {
-        return '';
-    }
+const s = {
+    a: '{{params.mp_app_id}}',
+    b: '{{params.query_page_num}}',
+    c: '{{params.business}}',
+    d: ['{{params.mp_app_id}}', 2, 3],
+    e: '{"app_id":{{params.mp_app_id}},"search_name":"","page_index":{{params.query_page_num}},"page_size":{{params.query_page_size}},"code":{{params.business.code}},"business": {{params.business}}}',
+    f: {
+        g: '{{params.mp_app_id}}',
+    },
+};
 
-    // 丢掉第一个元素
-    keys.shift();
-
-    let result = data;
-    keys.forEach((v) => {
-        result = typeof result === 'object' && Object.keys(result).includes(v) ? result[v] : '';
-    });
-    return result;
-}
+const result = convertJson(s, params);
+console.log(result);
